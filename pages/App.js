50268417cf106985081a6ab84f9632e25aae8eb5
T4P4N <tapank415@gmail.com>
2022-06-25 12:20:23 +0530

    added api

--- pages/App.js
+++ pages/App.js
@@ -19,22 +19,34 @@ export default function App() {
 	const [data, setData] = useState(null)
  	const resourceName = {singular:"product", plural:"products"};
 	const [productRows, setProductRows] = useState([])
+	const API_ENDPOINT = 'https://api.escuelajs.co/api/v1/products'
+	const [products, setProducts ] = useState([])
+	const [loading, setLoading ] = useState(true)
+	useEffect(()=>{
+		fetch(API_ENDPOINT)
+		      .then((res) => res.json())
+		      .then((data) => {
+		      	setProducts(data);
+		      	setLoading(false)
+		      })
+	},[])
+	
 
-	 const [product, setProduct] = useState(
-	 	[{
-	 	"id":1,
-	 	"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-	 	"price":109.95,
-	 	"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-	 	"category":"men's clothing",
-	 	"image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-	 	"rating":{"rate":3.9,"count":120}
-	 	},]
-	 	)
-	const { selectedResources, allResourcesSelected, handleSelectionChange } = useIndexResourceState(product);
+	 // const [product, setProduct] = useState(
+	 	// [{
+	 	// "id":1,
+	 	// "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+	 	// "price":109.95,
+	 	// "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+	 	// "category":"men's clothing",
+	 	// "image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+	 	// "rating":{"rate":3.9,"count":120}
+	 	// },]
+	 	// )
+	const { selectedResources, allResourcesSelected, handleSelectionChange } = useIndexResourceState(products);
 	 
-	 	const rowMarkup = product.map(
-	 	({id, title, price, description, cateory, image}, index)=>(
+	 	const rowMarkup = products.map(
+	 	({id, title, price, description, category, image}, index)=>(
 	 		<IndexTable.Row
 				id={id}
 				key={id}
@@ -52,6 +64,10 @@ export default function App() {
 	 			<IndexTable.Cell>
 	 				{price}
 	 			</IndexTable.Cell>
+
+	 			<IndexTable.Cell>
+	 				{category.name}
+	 			</IndexTable.Cell>
 			
 	 			<IndexTable.Cell>
 	 				{description}
@@ -66,16 +82,17 @@ export default function App() {
     		<Card title="Product List">
 					<IndexTable
 						resourceName={resourceName}
-						itemCount={product.length}
+						itemCount={products.length}
 						selectedItemsCount = {
 							allResourcesSelected ? "All" : selectedResources.length
 						}
 						onSelectionChange={handleSelectionChange}
 						headings={[
-						{title:'id'},
-						{title:'title'},
-						{title:'price'},
-						{title:'description'},
+						{title:'ID'},
+						{title:'Title'},
+						{title:'Price'},
+						{title:'Type'},
+						{title:'Description'},
 							
 						]}
 					>
